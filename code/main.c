72ff5ffca777436aace08dc3242a628e8ea22eeb
jmateo <justin_mateo@sfu.ca>
2022-11-10 22:58:21 -0800

    try

--- code/main.c
+++ code/main.c
@@ -3,24 +3,23 @@
 #include "includes/button.h"
 
 int main(){
-    // pthread_t nfc_t, motionSensor_t;
-    pthread_t motionSensor_t;
-    //NFC_descriptor nfc_desc;
+    pthread_t nfc_t, motionSensor_t;
+    NFC_descriptor nfc_desc;
     button_initiate();
 
-    // if (pthread_create(&nfc_t, NULL, &threadNFC, &nfc_desc) < 0){
-    //     perror("Could not create NFC thread\n");
-    //     return 1;
-    // } 
+    if (pthread_create(&nfc_t, NULL, &threadNFC, &nfc_desc) < 0){
+        perror("Could not create NFC thread\n");
+        return 1;
+    } 
     if (pthread_create(&motionSensor_t, NULL, &threadMotionSensor, NULL) < 0){
         perror("Could not create motion sensor thread\n");
         return 1;
     }
 
     while(!readButton(BUTTON_pathValue));
-    // pthread_cancel(nfc_t);
+    pthread_cancel(nfc_t);
     pthread_cancel(motionSensor_t);
-    // NFC_end(&nfc_desc);  
+    NFC_end(&nfc_desc);  
     printf("Ending... Goodbye!\n");
     return 0;
 }
