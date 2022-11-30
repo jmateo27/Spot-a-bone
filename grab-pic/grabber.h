0934215735eb6f1c8f80211596c8e8b8a1e486df
Avash <avash007thapa@gmail.com>
2022-11-30 00:19:15 -0800

    eh

---
+++ grab-pic/grabber.h
@@ -0,0 +1,33 @@
+/* V4L2 video picture grabber
+   Copyright (C) 2009 Mauro Carvalho Chehab <mchehab@infradead.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   Modified by Derek Molloy (www.derekmolloy.ie)
+   Modified to change resolution details and set paths for the Beaglebone.
+ */
+#ifndef GRABBER_H_
+#define GRABBER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <linux/videodev2.h>
+#include <libv4l2.h>
+
+static void xioctl(int fh, int request, void *arg);
+
+#endif
