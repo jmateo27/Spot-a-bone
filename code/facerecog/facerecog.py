b1859130c584e6e5ac7b2473fc3b48d95c317a55
Justin <jmateo@sfu.ca>
2022-11-30 00:20:12 -0800

    updated a few things

--- code/facerecog/facerecog.py
+++ code/facerecog/facerecog.py
@@ -14,8 +14,8 @@ import face_recognition
 class myThreads:
     def __init__(self):
         self.isStart = False
-        self.commandfile = "/mnt/remote/myApps/spotabone/analyzeFace.txt"
-        self.namefile = "/mnt/remote/myApps/spotabone/name.txt"
+        self.commandfile = "analyzeFace.txt"
+        self.namefile = "name.txt"
 
         # Initialize image encodings
         self.photos_dir = 'stock'
@@ -26,11 +26,13 @@ class myThreads:
         start_time = datetime.datetime.now()
 
         for p in os.listdir(self.photos_dir):
-            if (p.endswith('.jpg')):
+            if (p.endswith('.jpg') or p.endswith('.ppm')):
                 img = cv2.imread(self.photos_dir + '/' + p)
                 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 img_encoding = face_recognition.face_encodings(rgb_img)[0]
-                self.img_mapping.append( (img_encoding, p.replace('.jpg', '')) )
+                name = p.replace('.jpg', '').replace('.ppm', '')
+                print(name)
+                self.img_mapping.append( (img_encoding, name) )
         
         total_time = datetime.datetime.now() - start_time
         print(total_time.total_seconds() , "seconds to complete initialization")
@@ -58,7 +60,6 @@ class myThreads:
                 f.close()
             open(self.commandfile,"w").close()
 
-
     def analyze_photo(self):
         samples = []
         while(1):
@@ -70,10 +71,18 @@ class myThreads:
             start_time = datetime.datetime.now()
 
             for p in os.listdir(self.sample_dir):
-                if (p.endswith('.jpg')):
+                if (p.endswith('.jpg') or p.endswith('.ppm')):
                     samples.append(p)
 
+            if (len(samples) == 0):
+                print("Error: No valid input photos")
+                self.isStart = False
+                continue
+
             imgSample = cv2.imread(self.sample_dir + '/' + samples[0])
+            if (len(imgSample) == 0):
+                print("Error with the input photo")
+                return
             rgb_imgSample = cv2.cvtColor(imgSample, cv2.COLOR_BGR2RGB)
             imgSample_encoding = face_recognition.face_encodings(rgb_imgSample)[0]
 
@@ -90,16 +99,18 @@ class myThreads:
             for th in recog_threads:
                 th.join()
 
-            if (len(names) == 0):
-                print("No matches")
-
             total_time = datetime.datetime.now() - start_time
             print(total_time.total_seconds(), "seconds to analyze photo")
-            f = open(self.namefile, "w")
-            f.write(names[0] + '\n')
-            f.close()
+            if (len(names) == 0):
+                print("No matches")
+            else:
+                print(names[0])
+                f = open(self.namefile, "w")
+                f.write(names[0] + '\n')
+                f.close()
             
             self.isStart = False
+            samples = []
             print("Ready for a command")
             # should delete the photo from the samples folder, do later
 
