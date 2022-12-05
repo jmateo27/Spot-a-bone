4d2b3ec583817af5247b31bdc85480e07c31b051
Justin <jmateo@sfu.ca>
2022-12-04 20:02:20 -0800

    try this lol

--- code/cmodule/camera.c
+++ code/cmodule/camera.c
@@ -55,86 +55,87 @@ static void xioctl(int fh, int request, void *arg)
 
 int main(int argc, char **argv)
 {
-	struct v4l2_format fmt;
-	struct v4l2_buffer buf;
-	struct v4l2_requestbuffers req;
-	enum v4l2_buf_type type;
-	fd_set fds;
-	struct timeval tv;
-	int r, fd = -1;
-	unsigned int i, n_buffers;
-	char *dev_name = "/dev/video0";
-	char out_name[256], new_name[256];
-	FILE *fout;
-	struct buffer *buffers;
-
-	fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
-	if (fd < 0)
-	{
-		perror("Cannot open device");
-		exit(EXIT_FAILURE);
-	}
-
-	CLEAR(fmt);
-	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	fmt.fmt.pix.width = 1920;
-	fmt.fmt.pix.height = 1080;
-	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
-	fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
-	xioctl(fd, VIDIOC_S_FMT, &fmt);
-	if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_RGB24)
-	{
-		printf("Libv4l didn't accept RGB24 format. Can't proceed.\n");
-		exit(EXIT_FAILURE);
-	}
-	if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480))
-		printf("Warning: driver is sending image at %dx%d\n",
-			   fmt.fmt.pix.width, fmt.fmt.pix.height);
-
-	CLEAR(req);
-	req.count = 2;
-	req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	req.memory = V4L2_MEMORY_MMAP;
-	xioctl(fd, VIDIOC_REQBUFS, &req);
-
-	buffers = calloc(req.count, sizeof(*buffers));
-	for (n_buffers = 0; n_buffers < req.count; ++n_buffers)
-	{
-		CLEAR(buf);
-
-		buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		buf.memory = V4L2_MEMORY_MMAP;
-		buf.index = n_buffers;
-
-		xioctl(fd, VIDIOC_QUERYBUF, &buf);
-
-		buffers[n_buffers].length = buf.length;
-		buffers[n_buffers].start = v4l2_mmap(NULL, buf.length,
-											 PROT_READ | PROT_WRITE, MAP_SHARED,
-											 fd, buf.m.offset);
-
-		if (MAP_FAILED == buffers[n_buffers].start)
-		{
-			perror("mmap");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	for (i = 0; i < n_buffers; ++i)
-	{
-		CLEAR(buf);
-		buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		buf.memory = V4L2_MEMORY_MMAP;
-		buf.index = i;
-		xioctl(fd, VIDIOC_QBUF, &buf);
-	}
-	type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
 	while (1)
 	{
-		while (!isCommand());
+		while (!isCommand())
+			;
+		struct v4l2_format fmt;
+		struct v4l2_buffer buf;
+		struct v4l2_requestbuffers req;
+		enum v4l2_buf_type type;
+		fd_set fds;
+		struct timeval tv;
+		int r, fd = -1;
+		unsigned int i, n_buffers;
+		char *dev_name = "/dev/video0";
+		char out_name[256], new_name[256];
+		FILE *fout;
+		struct buffer *buffers;
+
+		fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
+		if (fd < 0)
+		{
+			perror("Cannot open device");
+			exit(EXIT_FAILURE);
+		}
+
+		CLEAR(fmt);
+		fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		fmt.fmt.pix.width = 1920;
+		fmt.fmt.pix.height = 1080;
+		fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
+		fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+		xioctl(fd, VIDIOC_S_FMT, &fmt);
+		if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_RGB24)
+		{
+			printf("Libv4l didn't accept RGB24 format. Can't proceed.\n");
+			exit(EXIT_FAILURE);
+		}
+		if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480))
+			printf("Warning: driver is sending image at %dx%d\n",
+				   fmt.fmt.pix.width, fmt.fmt.pix.height);
+
+		CLEAR(req);
+		req.count = 2;
+		req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		req.memory = V4L2_MEMORY_MMAP;
+		xioctl(fd, VIDIOC_REQBUFS, &req);
+
+		buffers = calloc(req.count, sizeof(*buffers));
+		for (n_buffers = 0; n_buffers < req.count; ++n_buffers)
+		{
+			CLEAR(buf);
+
+			buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+			buf.memory = V4L2_MEMORY_MMAP;
+			buf.index = n_buffers;
+
+			xioctl(fd, VIDIOC_QUERYBUF, &buf);
+
+			buffers[n_buffers].length = buf.length;
+			buffers[n_buffers].start = v4l2_mmap(NULL, buf.length,
+												 PROT_READ | PROT_WRITE, MAP_SHARED,
+												 fd, buf.m.offset);
+
+			if (MAP_FAILED == buffers[n_buffers].start)
+			{
+				perror("mmap");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		for (i = 0; i < n_buffers; ++i)
+		{
+			CLEAR(buf);
+			buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+			buf.memory = V4L2_MEMORY_MMAP;
+			buf.index = i;
+			xioctl(fd, VIDIOC_QBUF, &buf);
+		}
+		type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 		xioctl(fd, VIDIOC_STREAMON, &type);
-		
+
 		for (i = 0; i < 5; i++)
 		{
 			do
@@ -192,8 +193,9 @@ typedef int FD;
 bool isCommand()
 {
 	printf("checking...\n");
-	FD pFile = open(commandFile, O_RDWR );
-	if (!pFile){
+	FD pFile = open(commandFile, O_RDWR);
+	if (!pFile)
+	{
 		printf("ERROR: Unable to open file (%s) for read\n", commandFile);
 		exit(-1);
 	}
@@ -210,11 +212,13 @@ bool isCommand()
 	// Close
 	// fclose(pFile);
 	fclose(fopen(commandFile, "w")); // Clear the file
-	if (count > 0){
+	if (count > 0)
+	{
 		printf("there is a command");
 		return 1;
 	}
-	else{
+	else
+	{
 		return 0;
 	}
 }
