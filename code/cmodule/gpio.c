7b1418ac341f181ad7f0a07b8f8ba398e12f369a
jmateo <justin_mateo@sfu.ca>
2022-12-06 00:24:07 -0800

    no longer writing to the file

--- code/cmodule/gpio.c
+++ code/cmodule/gpio.c
@@ -1,4 +1,4 @@
- /* GPIO Interface C Sample Code */
+/* GPIO Interface C Sample Code */
 /* This code is intentionally left with room for improvement.
    Update this interface as needed. */
 #include <stdio.h>
@@ -22,63 +22,65 @@
 #define E_DIRECTION "/sys/class/gpio/gpio15/direction"
 #define E_VALUE "/sys/class/gpio/gpio15/value"
 
-static void writeToFile(const char* fileName, const char* value)
+static void writeToFile(const char *fileName, const char *value)
 {
-	FILE *pFile = fopen(fileName, "w");
-	fprintf(pFile, "%s", value);
-	fclose(pFile);
+    FILE *pFile = fopen(fileName, "w");
+    fprintf(pFile, "%s", value);
+    fclose(pFile);
 }
 
-void GPIO_writeDirection(int gpio, char* dir)
+void GPIO_writeDirection(int gpio, char *dir)
 {
-    char* fileName;
+    char *fileName;
 
-    switch (gpio) {
-        case D4_GPIO_NUMBER:
-            fileName = D4_DIRECTION;
-            break;
-        case D5_GPIO_NUMBER:
-            fileName = D5_DIRECTION;
-            break;
-        case D6_GPIO_NUMBER:
-            fileName = D6_DIRECTION;
-            break;
-        case D7_GPIO_NUMBER:
-            fileName = D7_DIRECTION;
-            break;
-        case RS_GPIO_NUMBER:
-            fileName = RS_DIRECTION;
-            break;
-        case E_GPIO_NUMBER:
-            fileName = E_DIRECTION;
-            break;
+    switch (gpio)
+    {
+    case D4_GPIO_NUMBER:
+        fileName = D4_DIRECTION;
+        break;
+    case D5_GPIO_NUMBER:
+        fileName = D5_DIRECTION;
+        break;
+    case D6_GPIO_NUMBER:
+        fileName = D6_DIRECTION;
+        break;
+    case D7_GPIO_NUMBER:
+        fileName = D7_DIRECTION;
+        break;
+    case RS_GPIO_NUMBER:
+        fileName = RS_DIRECTION;
+        break;
+    case E_GPIO_NUMBER:
+        fileName = E_DIRECTION;
+        break;
     }
     writeToFile(fileName, dir);
 }
 
-void GPIO_writeValue(int gpio, char* val)
+void GPIO_writeValue(int gpio, char *val)
 {
-    char* fileName;
+    char *fileName;
 
-    switch (gpio) {
-        case D4_GPIO_NUMBER:
-            fileName = D4_VALUE;
-            break;
-        case D5_GPIO_NUMBER:
-            fileName = D5_VALUE;
-            break;
-        case D6_GPIO_NUMBER:
-            fileName = D6_VALUE;
-            break;
-        case D7_GPIO_NUMBER:
-            fileName = D7_VALUE;
-            break;
-        case RS_GPIO_NUMBER:
-            fileName = RS_VALUE;
-            break;
-        case E_GPIO_NUMBER:
-            fileName = E_VALUE;
-            break;
+    switch (gpio)
+    {
+    case D4_GPIO_NUMBER:
+        fileName = D4_VALUE;
+        break;
+    case D5_GPIO_NUMBER:
+        fileName = D5_VALUE;
+        break;
+    case D6_GPIO_NUMBER:
+        fileName = D6_VALUE;
+        break;
+    case D7_GPIO_NUMBER:
+        fileName = D7_VALUE;
+        break;
+    case RS_GPIO_NUMBER:
+        fileName = RS_VALUE;
+        break;
+    case E_GPIO_NUMBER:
+        fileName = E_VALUE;
+        break;
     }
     writeToFile(fileName, val);
 }
